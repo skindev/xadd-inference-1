13ef79a3f735f2243e69d5d9561de91c8a333919
S K <skin.dev.null@gmail.com>
2016-08-25 16:14:04 +1000

    Update Main to handle OE domain

--- src/cmomdp/Main.java
+++ src/cmomdp/Main.java
@@ -50,9 +50,14 @@ public class Main {
         HashMap<String, Boolean> subsMapBoolean = new HashMap<String, Boolean>();
         HashMap<String, ExprLib.ArithExpr> subsMap = new HashMap<String, ExprLib.ArithExpr>();
 
-//        subsMapBoolean.put("eps", Boolean.TRUE);
-//        subsMap.put("i", new ExprLib.DoubleExpr(100.0));
-//        subsMap.put("r", new ExprLib.DoubleExpr(0.01));
+        subsMapBoolean.put("eps", Boolean.TRUE);
+        subsMapBoolean.put("eta", Boolean.TRUE);
+        subsMap.put("i", new ExprLib.DoubleExpr(100.0));
+        subsMap.put("r", new ExprLib.DoubleExpr(0.01));
+        subsMap.put("perm_price", new ExprLib.DoubleExpr(0.01));
+        subsMap.put("temp_price", new ExprLib.DoubleExpr(0.01));
+        subsMap.put("inventory", new ExprLib.DoubleExpr(0.01));
+        subsMap.put("capture", new ExprLib.DoubleExpr(0.01));
 
         // Initialise a connection to the Non-linear solver
         Optimise.RegisterOptimisationMethod(new MATLABNonLinear());
